bfb281a4a2e731e6218a8965eca6670541e37b20
Ronald González <akuma424424@gmail.com>
2025-09-14 08:05:41 +0000

    Administrar tokens de depuración Los tokens de depuración permiten que l

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,6 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,6 +20,23 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Initialize App Check
+if (typeof window !== 'undefined') {
+  const appCheckDebugToken = process.env.NEXT_PUBLIC_APP_CHECK_DEBUG_TOKEN;
+  
+  if (process.env.NODE_ENV === 'development' && appCheckDebugToken) {
+    self.FIREBASE_APPCHECK_DEBUG_TOKEN = appCheckDebugToken;
+  }
+  
+  initializeAppCheck(app, {
+    // TODO: Replace with your reCAPTCHA v3 site key
+    provider: new ReCaptchaV3Provider('6Ld-............-..........._....'),
+    isTokenAutoRefreshEnabled: true
+  });
+}
+
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
